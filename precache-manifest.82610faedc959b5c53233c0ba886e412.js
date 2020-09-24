e9c969817c51684e0dcbc10ea35293bc873dd9c5
unknown <ahmedjodaalmory99@gmail.com>
2020-09-25 00:31:14 +0200

    Updates

---
+++ precache-manifest.82610faedc959b5c53233c0ba886e412.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08547fd255655243b4a5316bc4898fb",
+    "url": "/soto/index.html"
+  },
+  {
+    "revision": "b49b84e9cfdad1ab9ed6",
+    "url": "/soto/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b4793fae4dd1ef97f8ed",
+    "url": "/soto/static/css/main.4cd2bd58.chunk.css"
+  },
+  {
+    "revision": "b49b84e9cfdad1ab9ed6",
+    "url": "/soto/static/js/2.87c65f6f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/soto/static/js/2.87c65f6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4793fae4dd1ef97f8ed",
+    "url": "/soto/static/js/main.b464212c.chunk.js"
+  },
+  {
+    "revision": "c64e53cbfa20e32ebbc5",
+    "url": "/soto/static/js/runtime-main.b03ec50c.js"
+  },
+  {
+    "revision": "0eac9069fc8b678d632d981a073e79e8",
+    "url": "/soto/static/media/Layer1.0eac9069.png"
+  },
+  {
+    "revision": "88cc68185feee6eb6da70761c67ae9e7",
+    "url": "/soto/static/media/ocaen.88cc6818.jpg"
+  },
+  {
+    "revision": "dfe7ff2e51bc4c42cc1432b0e307f4bc",
+    "url": "/soto/static/media/soto.dfe7ff2e.png"
+  }
+]);
